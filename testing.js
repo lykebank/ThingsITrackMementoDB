e31ff9c4eec686ef950ccd2a1a62ee60067039f3
Kyle Knab <knab_kyle@bah.com>
2022-08-03 14:10:08 -0400

    destructing testing added space

--- testing.js
+++ testing.js
@@ -1,6 +1,9 @@
 function test(){}
 
-test.prototype.destructuring = function({name = 'kyle'} = {}){
+test.prototype.destructuring = function( {name = 'kyle'} = {}){
+    if(log && typeof log === 'function'){
+        log('name: ' + name);
+    }
     if(console){
         console.log('name: ' + name);
     }
